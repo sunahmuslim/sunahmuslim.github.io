1ebd2f387be24f14b51ba665c1ef0a7e397cfa32
Restu Suhendar <restu@jejakin.com>
2026-09-21 21:57:33 +0700

    deploy: update static export for sunahmuslim.com (2026-09-21T14:57:32.396Z)

---
+++ _next/static/chunks/2i6px3tyl_ccl.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,70185,t=>{t.v(c=>Promise.all(["static/chunks/0mcj00fjnhfqz.js"].map(c=>t.l(c))).then(()=>c(43677)))}]);
